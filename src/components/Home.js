905e105d1fdabf01439db295246b16e629b36154
tollispa <112690202+tollispa@users.noreply.github.com>
2023-04-20 13:08:24 +0200

    changed to full-height on page

--- src/components/Home.js
+++ src/components/Home.js
@@ -41,7 +41,7 @@
    }
     
     return (
-        <div className="h-screen w-full bg-black relative text-white">
+        <div className="h-full w-full bg-black relative text-white">
           <h1 className="text-3xl text-green-300 font-bold p-2 h-[100px]" >P3 dagens radio-tablå</h1>
           <p className="font-bold color-gray-800">Av Tollis Papadopoulos</p>
          <div className="absolute top-5 right-5 hidden sm:block">
